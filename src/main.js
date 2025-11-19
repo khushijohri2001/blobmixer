abdab4b7d2d4609e1dbe8a4f3e1634fdcea9b83f
Khushi Johri <khushijohri2001@gmail.com>
2025-11-19 18:47:16 +0530

    finished

--- src/main.js
+++ src/main.js
@@ -1,50 +1,78 @@
 import * as THREE from "three";
 import { EXRLoader, OrbitControls } from "three/examples/jsm/Addons.js";
-import CustomShaderMaterial from "three-custom-shader-material/vanilla"
+import CustomShaderMaterial from "three-custom-shader-material/vanilla";
 import { mergeVertices } from "three/examples/jsm/utils/BufferGeometryUtils.js";
 import vertexShader from "./shaders/vertex.glsl";
+import textVertexShader from "./shaders/textVertex.glsl";
+import textFragmentShader from "./shaders/textFragment.glsl";
+import { Text } from "troika-three-text";
+import { blobs } from "../public/configVal";
+import gsap from "gsap";
+
+const loadingManager = new THREE.LoadingManager();
+const rgbeLoader = new EXRLoader(loadingManager);
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+let isAnimating = false;
+let currentIndex = 0;
+let currentBlob = blobs[currentIndex].config;
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(blobs[currentIndex].background);
 const camera = new THREE.PerspectiveCamera(
   50,
-  window.innerWidth/window.innerHeight,
+  window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
 camera.position.z = 5;
 
+
+
 const canvas = document.querySelector("canvas");
-const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-
 const ambientLight = new THREE.AmbientLight("white", 1);
-scene.add(ambientLight)
+scene.add(ambientLight);
 
+const uniforms = {
+  uTime: { value: 0 },
+  uPositionFrequency: { value: currentBlob.uPositionFrequency },
+  uPositionStrength: { value: currentBlob.uPositionStrength },
+  uTimeFrequency: { value: 1 },
+  uSmallWavePositionFrequency: { value:currentBlob.uSmallWavePositionFrequency },
+  uSmallWavePositionStrength: { value: currentBlob.uSmallWavePositionStrength },
+  uSmallWaveTimeFrequency: { value: 0.5 },
+};
 
-const geometry = new THREE.IcosahedronGeometry(1, 64);
+const geometry = new THREE.IcosahedronGeometry(1.2, 64);
 const material = new CustomShaderMaterial({
- baseMaterial: THREE.MeshPhysicalMaterial,
- vertexShader,
- color: "pink",
-  roughness: 0.1,
-  metalness: 1,
-  reflectivity: 0.5,
-  transmission: 1,
-  transparent: true
-})
+  baseMaterial: THREE.MeshPhysicalMaterial,
+  vertexShader,
+  uniforms,
+  map: textureLoader.load(`./gradient/${currentBlob.map}.png`),
+  roughness: currentBlob.roughness,
+  metalness: currentBlob.metalness,
+  envMapIntensity: currentBlob.envMapIntensity,
+  transmission: currentBlob.transmission,
+  clearcoat: currentBlob.clearcoat,
+  clearcoatRoughness: currentBlob.clearcoatRoughness,
+  flatShading: currentBlob.flatShading,
+  wireframe: currentBlob.wireframe,
+});
 const mergedGeometry = mergeVertices(geometry);
 mergedGeometry.computeTangents();
 const sphere = new THREE.Mesh(mergedGeometry, material);
 
 scene.add(sphere);
 
-const rgbeLoader = new EXRLoader();
 rgbeLoader.load(
-"https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/1k/brown_photostudio_01_1k.exr",  function (texture) {
+  "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/1k/brown_photostudio_01_1k.exr",
+  function (texture) {
     texture.mapping = THREE.EquirectangularReflectionMapping;
     scene.environment = texture;
   },
@@ -54,22 +82,147 @@ rgbeLoader.load(
   }
 );
 
+const textMaterial = new THREE.ShaderMaterial({
+  vertexShader: textVertexShader,
+  fragmentShader: textFragmentShader,
+  side: THREE.DoubleSide,
+  uniforms: {
+    progress: { value: 0.0 },
+    direction: { value: 1 },
+  },
+});
+
+const texts = blobs.map((blob, index) => {
+  const myText = new Text();
+  myText.text = blob.name;
+  myText.font = `./aften_screen.woff`;
+  myText.anchorX = "center";
+  myText.anchorY = "middle";
+  myText.material = textMaterial;
+  myText.position.set(0, 0, 2);
+  if (index !== 0) myText.scale.set(0, 0, 0);
+  myText.letterSpacing = -0.08;
+  myText.fontSize = window.innerWidth / 3000;
+  myText.glyphGeometryDetail = 20;
+  myText.outlineColor = new THREE.Color(0x111111);
+  myText.outlineWidth = 0.01;
+  myText.outlineBlur = 0.03;
+  myText.outlineOpacity = 0.15;
+  myText.sync();
+  scene.add(myText);
+  return myText;
+});
+
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.damping = true;
 controls.dampingFactor = 0.2;
+controls.enablePan = false;
+controls.enableZoom = false;
 
-window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth/ window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+
+window.addEventListener("wheel", (e) => {
+  // Already animating? Ignore new scrolls
+  if (isAnimating) return;
+
+  // Ignore micro scrolls (trackpad bounce)
+  if (Math.abs(e.deltaY) < 30) return;
+
+  isAnimating = true;
+
+  const direction = Math.sign(e.deltaY); // 1 for down, -1 for up
+  const next = (currentIndex + direction + blobs.length) % blobs.length;
+
+  texts[next].scale.set(1, 1, 1);
+  texts[next].position.x = direction * 4;
+
+  gsap.to(textMaterial.uniforms.progress, {
+    value: 0.5,
+    duration: 1,
+    ease: "linear",
+    onComplete: () => {
+      currentIndex = next;
+      textMaterial.uniforms.progress.value = 0;
+
+      // unlock only AFTER animation completes
+      setTimeout(() => {
+        isAnimating = false;
+      }, 500);
+    },
+  });
+
+gsap.to(texts[currentIndex].position, {
+  x: -direction*4,
+  duration: 1,
+  ease: "power2.inOut",
 })
 
-function animate(){
-  requestAnimationFrame(animate);
-  sphere.rotation.y += 0.005;
-  controls.update();
-  renderer.render(scene, camera);
+gsap.to(texts[next].position, {
+  x: 0,
+  duration: 1,
+  ease: "power2.inOut",
+})
+
+let targetY = sphere.rotation.y + Math.PI * -direction * 4;
+
+gsap.to(sphere.rotation, {
+  y: targetY,
+  duration: 1,
+  ease: "power2.inOut"
+});
+
+
+const bg = new THREE.Color(blobs[next].background);
+gsap.to(scene.background, {
+  r: bg.r,
+  g: bg.g,
+  b: bg.b,
+  duration: 1,
+  ease: "power2.inOut",
+})
+
+updateBlob(blobs[next].config);
+
+});
+
+function updateBlob(config) {
+  if(!config) return;
+
+  if(config.uPositionFrequency !== undefined) gsap.to( uniforms.uPositionFrequency, { value: config.uPositionFrequency, duration: 1, ease: "power2.inOut" });
+  if(config.uPositionStrength !== undefined) gsap.to( uniforms.uPositionStrength, { value: config.uPositionStrength, duration: 1, ease: "power2.inOut" });
+  if(config.uSmallWavePositionFrequency !== undefined) gsap.to( uniforms.uSmallWavePositionFrequency, { value: config.uSmallWavePositionFrequency, duration: 1, ease: "power2.inOut" });
+  if(config.uSmallWavePositionStrength !== undefined) gsap.to( uniforms.uSmallWavePositionStrength, { value: config.uSmallWavePositionStrength, duration: 1, ease: "power2.inOut" });
+  
+  // Update material properties
+  if(config.map) {
+    setTimeout(() => {
+      material.map = textureLoader.load(`./gradient/${config.map}.png`)
+    }, 500);
+  };
+  if(config.roughness !== undefined) gsap.to( material, { roughness: config.roughness, duration: 1, ease: "power2.inOut" });
+  if(config.metalness !== undefined) gsap.to( material, { metalness: config.metalness, duration: 1, ease: "power2.inOut" });
+  if(config.envMapIntensity !== undefined) gsap.to( material, { envMapIntensity: config.envMapIntensity, duration: 1, ease: "power2.inOut" });
+  if(config.transmission !== undefined) gsap.to( material, { transmission: config.transmission, duration: 1, ease: "power2.inOut" });
+  if(config.clearcoat !== undefined) gsap.to( material, { clearcoat: config.clearcoat, duration: 1, ease: "power2.inOut" });
+  if(config.clearcoatRoughness !== undefined) gsap.to( material, { clearcoatRoughness: config.clearcoatRoughness, duration: 1, ease: "power2.inOut" });
+  if(config.flatShading !== undefined) material.flatShading = config.flatShading;
+  if(config.wireframe !== undefined) material.wireframe = config.wireframe;
 }
 
-animate();
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
+const clock = new THREE.Clock();
+
+loadingManager.onLoad = () => {
+  function animate() {
+    requestAnimationFrame(animate);
+    uniforms.uTime.value = clock.getElapsedTime() * 0.1;
+    controls.update();
+    renderer.render(scene, camera);
+  }
+
+  animate();
+};
---
+++ src/shaders/textVertex.glsl
@@ -0,0 +1,26 @@
+uniform float progress;
+uniform float direction;
+varying vec2 vUv;
+
+#define PI 3.1415926538
+
+vec3 rotateAxis(vec3 p, vec3 axis, float angle) {
+    return mix(dot(axis, p) * axis, p, cos(angle)) + cross(axis, p) * sin(angle);
+}
+
+void main() {
+    vUv = uv;
+    vec3 pos = position;
+
+    float twirlPeriod = sin(progress * PI * 2.);
+
+    float rotateAngle = -direction * pow(sin(progress * PI), 1.5) * PI * 2.;
+
+    float twirlAngle = -sin(uv.x - .5) * pow(twirlPeriod, 2.0) * -4.;
+
+    float twirlRotate = rotateAngle + twirlAngle;
+
+    pos = rotateAxis(pos, vec3(1., 0., 0.), twirlRotate);
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+}
